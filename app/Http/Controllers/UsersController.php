1e7a0ce6d4e40fba840ad8f8c3dc3e554713525b
Twist <911754664@qq.com>
2020-02-18 03:44:52 +0000

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -18,4 +18,15 @@ class UsersController extends Controller
         # code...
         return view('users.show', compact('user'));
     }
+
+    public function store(Request $request)
+    {
+        # code...
+        $this->validate($request,[
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
+    }
 }
