91508acb596085aa19ca3246e1196cdc074bc343
Twist <911754664@qq.com>
2020-02-19 07:50:13 +0000

    管理员可删除用户

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -91,4 +91,13 @@ class UsersController extends Controller
         return redirect()->route('users.show',$user->id);
     }
 
+    public function destroy(User $user)
+    {
+        # code...
+        $this->authorize('destroy',$user);
+        $user->delete();
+        session()->flash('success','成功删除用户');
+        return back();
+    }
+
 }
--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -24,5 +24,13 @@ class UserPolicy
         # code...
         return $currentUser->id === $user->id;
     }
+
+    //只有当前用户拥有管理员权限且删除的用户不是自己时才显示链接。
+    public function destroy(User $currentUser, User $user)
+    {
+        return $currentUser->is_admin && $currentUser->id !== $user->id;
+    }
+
+
 }
 
